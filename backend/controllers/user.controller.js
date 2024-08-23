27ecddc6dac7f63c84c22ef399c9d44fea115ccc
pawan bhandari <bhandaripawan769@gmail.com>
2024-08-23 22:09:07 +0545

    added all models and the user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,108 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(401).json({
+        message: "Please,fill all of the fields!",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(401).json({
+        message: "User already registered!",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "Please fill all the fields",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Incorrect username or password!",
+        success: false,
+      });
+    }
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      return res.status(401).json({
+        message: "Incorrect username or password!",
+        success: false,
+      });
+    }
+    user = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      profilePic: user.profilePic,
+      bio: user.bio,
+      followers: user.followers,
+      following: user.following,
+      posts: user.posts,
+      bookmarks: user.bookmarks,
+    };
+    const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        message: "logged in successfully",
+        success: true,
+        user,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = async (_, res) => {
+  try {
+    return res.cookie("token", "", { maxAge: 0 }).json({
+      message: "logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProfile = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    let user = await User.findOne(userId);
+    return res.status(200).json({
+      user,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
